cff2b0c0b7927c4b3469f543301f94d967197d1d
Haochuan Wei <haochuanwei@yahoo.com>
2020-01-22 22:19:30 -0800

    Reorganized tests

--- test_sample.py
+++ test_sample.py
@@ -14,8 +14,6 @@ def foo(a, b, add=True):
     else:
         return a - b
 
-foo_callback = Callback.wrap(foo)
-
 class Bar:
     def __init__(self):
         self.value = 0
@@ -23,35 +21,40 @@ class Bar:
     def add(self, a, b):
         return a + b
 
-bar_callback = Callback.wrap(Bar().add)
-
-def test_callback_functionality():
+def test_basic_components():
     assert foo(1, 2) == 3
+
+def test_function_callback():
+    foo_callback = Callback.wrap(foo)
     assert foo_callback(2, 1, add=False)() == 1
 
 def test_method_callback():
+    bar_callback = Callback.wrap(Bar().add)
     assert bar_callback(1, 2)() == 3
 
-def test_callback_session_functionality():
+def test_additional_prereq():
+    pass
+
+def test_callback_session():
     session = CallbackSession()
     foo_sess = session.wrap(foo)
     callback = foo_sess(1, 2)
-    session.compile()
     session.run()
     assert len(session.lookup) == 1
     assert session.lookup[callback._uuid] == 3
 
 def test_parallel_callback():
+    foo_callback = Callback.wrap(foo)
     single_run = running_time(foo_callback(1, 1))
 
     tic = time.time()
     i_dim, j_dim = 20, 20
     assert i_dim * j_dim > 2
     session = CallbackSession()
+    foo_sess = session.wrap(foo)
     for i in range(i_dim):
         for j in range(j_dim):
-            session.add(foo_callback(i, j))
-    session.compile()
+            callback = foo_sess(i, j)
     session.run()
     toc = time.time()
     parallel_run = toc - tic
@@ -59,6 +62,7 @@ def test_parallel_callback():
     assert parallel_run < single_run * i_dim * j_dim
 
 def test_serial_callback():
+    foo_callback = Callback.wrap(foo)
     single_run = running_time(foo_callback(0.0001, 0.0))
 
     tic = time.time()
@@ -69,7 +73,6 @@ def test_serial_callback():
         increment = i * 0.0001
         callback_sum = foo_callback(callback_sum, increment)
         session.add(callback_sum)
-    session.compile()
     session.run()
     toc = time.time()
     serial_run = toc - tic
@@ -77,3 +80,5 @@ def test_serial_callback():
     assert 0.4950 - 1e-8 <= session.lookup[callback_sum._uuid] < 0.4950 + 1e-8
     assert single_run * i_dim < serial_run < 1.5 * single_run * i_dim
 
+if __name__ == '__main__':
+    test_parallel_callback()
